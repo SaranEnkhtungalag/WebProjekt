98d415fe03b837c19618013fd5ed15bd62aff7d8
SaranEnkhtungalag <tungaa.love61@gmail.com>
2023-10-18 15:01:10 +0200

    Hello World

---
+++ src/main/java/htwberlin/webtech_Projekt/WebtechProjektApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.webtech_Projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechProjektApplication.class, args);
+	}
+
+}
---
+++ src/main/java/htwberlin/webtech_Projekt/web/HelloWorldController.java
@@ -0,0 +1,19 @@
+package htwberlin.webtech_Projekt.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
+        return "Hello " + name;
+    }
+
+}
