0ecef85ad2a2cc0c6befa1b72e3ef56442bc349c
SaranEnkhtungalag <tungaa.love61@gmail.com>
2023-11-01 10:26:54 +0100

    Datenbank Zugriff

--- src/main/java/htwberlin/webtech_Projekt/web/HelloWorldController.java
+++ src/main/java/htwberlin/webtech_Projekt/web/HelloWorldController.java
@@ -3,6 +3,7 @@ package htwberlin.webtech_Projekt.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,5 +15,4 @@ public class HelloWorldController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
         return "Hello " + name;
     }
-
 }
